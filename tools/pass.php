c5b25e91639c57836df9f4db42df1f434b962966
wm2zx <2577802736@qq.com>
2022-05-22 15:39:27 +0800

    Site updated: 2022-05-22 15:39:26

---
+++ tools/pass.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>全球被黑站点统计系统 - Hacked.com.cn</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="icon" href="/favicon.ico">
+<meta name="keywords" content="被黑网站,被黑站点,被黑统计,被黑查询,黑页提交,提交黑页,黑页统计,被黑网站提交,被黑网站查询,中国被黑站点,被黑网站统计,被黑站点统计,全球被黑站点统计,被黑网站统计系统,中国被黑站点排行" />
+<meta name="description" content="﻿本站主要通过网络搜集国内被黑网站信息，统计分析数据，为部署安全型网络提供强有力的依据.本站所有工作人员均不参与黑站，挂马或赢利性行为，所有数据均为网民提供，提交者不一定是黑站人，所有提交采取不记名，先提交先审核的方式，如有任何疑问请及时与我们联系。" />
+<link href="https://www.hacked.com.cn/css/style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.password { margin-left:30px; }
+-->
+</style>
+
+</head>
+<body>  
+<div id="content">
+        <div id="logo">
+            <a href="index.php"><img src="https://www.hacked.com.cn/images/logo.gif" border="0" /></a>
+        </div>
+<div id="menu">
+            <ul>
+                <li><a href="/">首页</a></li>
+<li><a href="https://www.hacked.com.cn/news.php">新闻</a></li>
+                <li><a href="https://www.hacked.com.cn/hackers.php">排行榜</a></li>
+                <li><a href="https://www.hacked.com.cn/submit.php">提交 <img src="https://www.hacked.com.cn/images/star.gif" border="0"></a></li>
+                <li><a href="https://www.hacked.com.cn/country.php">国别统计</a></li>
+                <li><a href="https://www.hacked.com.cn/tools.php">在线工具</a></li>
+                <li><a href="https://www.hacked.com.cn/stats.php">统计地图</a></li>
+                <li><a href="https://www.hacked.com.cn/disclaimer.php">免责申明</a></li>
+                <li><a href="https://www.hacked.com.cn/count.php">统计</a></li>
+                <li style="float:right;">
+                  <form name="sitesearch" action="search.php" method="get">
+                  <input id="var" type="text" name="var" value="search..." style="width: 98px" onclick="window.document.sitesearch.var.value=''"/></form            </ul>
+        </div>
+
+
+<div class="bodyy">
+
+            <div id="news">
+<div class="password">
+</br></br></br>##############密码安全检测##############</br>#&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp#</br>#&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp#</br>#&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp提示：你没有输入任何参数！&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp#</br>#&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp#</br>#&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp#</br>##############密码安全检测##############</br></br>
+</br>
+###########以下为TOP100弱口令原文###########
+</br>
+123456
+</br>
+123456.
+</br>
+a123456
+</br>
+a123456.
+</br>
+123456a
+</br>
+123456a.
+</br>
+123456abc
+</br>
+123456abc.
+</br>
+abc123456
+</br>
+abc123456.
+</br>
+woaini1314
+</br>
+woaini1314.
+</br>
+qq123456
+</br>
+qq123456.
+</br>
+woaini520
+</br>
+woaini520.
+</br>
+woaini123
+</br>
+woaini123.
+</br>
+woaini521
+</br>
+woaini521.
+</br>
+qazwsx
+</br>
+qazwsx.
+</br>
+1qaz2wsx
+</br>
+1qaz2wsx.
+</br>
+1q2w3e4r
+</br>
+1q2w3e4r.
+</br>
+1q2w3e4r5t
+</br>
+1q2w3e4r5t.
+</br>
+1q2w3e
+</br>
+1q2w3e.
+</br>
+qwertyuiop
+</br>
+qwertyuiop.
+</br>
+zxcvbnm
+</br>
+zxcvbnm.
+</br>
+123456
+</br>
+a123456
+</br>
+123456a
+</br>
+5201314
+</br>
+111111
+</br>
+woaini1314
+</br>
+qq123456
+</br>
+123123
+</br>
+000000
+</br>
+1qaz2wsx
+</br>
+1q2w3e4r
+</br>
+qwe123
+</br>
+7758521
+</br>
+123qwe
+</br>
+a123123
+</br>
+123456aa
+</br>
+woaini520
+</br>
+woaini
+</br>
+100200
+</br>
+1314520
+</br>
+woaini123
+</br>
+123321
+</br>
+q123456
+</br>
+123456789
+</br>
+123456789a
+</br>
+5211314
+</br>
+asd123
+</br>
+a123456789
+</br>
+z123456
+</br>
+asd123456
+</br>
+a5201314
+</br>
+aa123456
+</br>
+zhang123
+</br>
+aptx4869
+</br>
+123123a
+</br>
+1q2w3e4r5t
+</br>
+1qazxsw2
+</br>
+5201314a
+</br>
+1q2w3e
+</br>
+aini1314
+</br>
+31415926
+</br>
+q1w2e3r4
+</br>
+123456qq
+</br>
+woaini521
+</br>
+1234qwer
+</br>
+a111111
+</br>
+520520
+</br>
+iloveyou
+</br>
+abc123
+</br>
+110110
+</br>
+111111a
+</br>
+123456abc
+</br>
+w123456
+</br>
+7758258
+</br>
+123qweasd
+</br>
+159753
+</br>
+qwer1234
+</br>
+a000000
+</br>
+qq123123
+</br>
+zxc123
+</br>
+123654
+</br>
+abc123456
+</br>
+123456q
+</br>
+qq5201314
+</br>
+12345678
+</br>
+000000a
+</br>
+456852
+</br>
+as123456
+</br>
+1314521
+</br>
+112233
+</br>
+521521
+</br>
+qazwsx123
+</br>
+zxc123456
+</br>
+abcd1234
+</br>
+asdasd
+</br>
+666666
+</br>
+love1314
+</br>
+QAZ123
+</br>
+aaa123
+</br>
+q1w2e3
+</br>
+aaaaaa
+</br>
+a123321
+</br>
+123000
+</br>
+11111111
+</br>
+12qwaszx
+</br>
+5845201314
+</br>
+s123456
+</br>
+nihao123
+</br>
+caonima123
+</br>
+zxcvbnm123
+</br>
+wang123
+</br>
+159357
+</br>
+1A2B3C4D
+</br>
+asdasd123
+</br>
+584520
+</br>
+753951
+</br>
+147258
+</br>
+1123581321
+</br>
+110120
+</br>
+qq1314520
+</br>
+###########以上为TOP100弱口令原文###########
+</br>
+</br>
+</br>
+Powered by:Hacked.com.cn
+</br>
+</br>
+ </div>
+                <br />
+            </div>
+        </div>
+		<div id="footer">
+            <p align="center">
+页面总访问量:5981757(PV) 页面执行时间:0.12834(MS)
+                </p>
+                <p align="center">
+                    归属 - 非商业 - NoDerivs 3.0 Unported 许可证<script src="https://s19.cnzz.com/z_stat.php?id=1263217872&web_id=1263217872" language="JavaScript"></script><img src="images/star.gif" border="0">
+                </p>
+        </div>
+    </div>
+</body>
+
+</html>
